cbf688346a26d5842ba1790203c4bda719d029f8
Max Lechner <mkonlech@gmail.com>
2021-03-01 22:12:43 -0800

    update page with table vs. card structure

--- src/components/APIcontainer.js
+++ src/components/APIcontainer.js
@@ -1,72 +1,75 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import API from "../utils/API";
 
 
 function APIcontainer() {
+
   const [employees, setEmployees] = useState([]);
-  // const [all, setAll] = useState(false);
+  const [filter, setFilter] = useState("");
+
+  
+
   
   useEffect(() => {
-      API.search()
-        .then((res) => {
-          if (res.data.length === 0) throw new Error("No results found.");
-          if (res.data.status === "error") throw new Error(res.data.message);
+    API.search()
+      .then((res) => {
+        if (res.data.length === 0) throw new Error("No results found.");
+        if (res.data.status === "error") throw new Error(res.data.message);
 
-          setEmployees(res.data.results);
-        })
-        .catch((err) => console.log(err));
-    }, []);
-    
-    function nameButton() {
-
-      employees.sort();
-
-    };
-
-    function locationButton() {
-
-      employees.sort();
-
-    };
+        setEmployees(res.data.results);
+      })
+      .catch((err) => console.log(err));
+  }, []);
       
-    return (
-      
-      <div classname='card'>
-        <h1>Employee list:</h1>
-
-        <p>
-          Click one of the buttons below to filter the employees:
-        </p>
-        
-        <button onClick={nameButton} type="button" className="btn btn-success">Order by name</button>
-        <button onClick={locationButton} type="button" className="btn btn-warning">Order by location</button>
-
-      <div className='card-body' style={{ display: "flex", flexWrap: "wrap" }}>
-        {employees.map((employee) => (
-            <div
-            key={employee.phone}
-            className='card-body'
-            style={{ flex: "flex", maxWidth: "50%" }}
-            >
-              <img
-                alt='employee portrait'
-                src={employee.picture.large}
-                style={{ maxWidth: "500px", borderRadius: "1px" }}
-              />
-              <h3 className='card-text'>
-                {employee.name.first} {employee.name.last}
-              </h3>
-              <h5 className='card-text'>City: {employee.location.city}</h5>
-              <h5 className='card-text'>Country: {employee.location.country}</h5>
-              <h5 className='card-text'>Phone: {employee.phone}</h5>
-              <h5 className='card-text'>Username: {employee.login.username}</h5>
-              <h5 className='card-text'><a href={employee.email}>{employee.email}</a>
-              </h5>
-          </div>
-        ))}
-
-      </div>
-
+  return (
+    <div>
+      <h1>Employee list:</h1>
+        <table class="table table-bordered table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>City</th>
+              <th>Country</th>
+              <th>Phone</th>
+              <th>Username</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee.phone}>
+                <td>
+                  <img
+                    alt='employee portrait'
+                    src={employee.picture.large}
+                    style={{ maxWidth: "500px", borderRadius: "1px" }}
+                  />
+                </td>
+                <td>
+                  <p>
+                    {employee.name.first} {employee.name.last}
+                  </p>
+                </td>
+                <td>
+                  <p >{employee.location.city}</p>
+                </td>
+                <td>
+                  <p >{employee.location.country}</p>
+                </td>
+                <td>
+                  <p >{employee.phone}</p>
+                </td>
+                <td>
+                  <p >{employee.login.username}</p>
+                </td>
+                <td>
+                  <p ><a href={employee.email}>{employee.email}</a></p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
     
